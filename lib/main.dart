9972a63bb6c3ef6305a7b2dfa19f34d6d460fe6d
PerairaCC <carloscoto0103@gmail.com>
2021-05-23 12:52:13 -0600

    Clase 2 Seccion 17

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:mapas_app/pages/acceso_gps_page.dart';
+import 'package:mapas_app/pages/loading_page.dart';
+import 'package:mapas_app/pages/mapa_page.dart';
  
 void main() => runApp(MyApp());
  
@@ -8,16 +11,12 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Material App',
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
-        ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
-        ),
-      ),
+      home: LoadingPage(),
+      routes: {
+        'mapa'       : ( _ ) => MapaPage(),
+        'loading'    : ( _ ) => LoadingPage(),
+        'acceso_gps' : ( _ ) => AccesoGpsPage(),
+      },
     );
   }
 }
---
+++ lib/pages/loading_page.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:mapas_app/helpers/helpers.dart';
+import 'package:mapas_app/pages/mapa_page.dart';
+
+
+class LoadingPage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: FutureBuilder(
+        future: checkGpsYLocation(context),
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          return Center(
+            child: CircularProgressIndicator(strokeWidth: 2),
+          );
+        },
+      ),
+   );
+  }
+
+  Future checkGpsYLocation(BuildContext context) async{
+
+    //TODO: PermisoGPS
+    //TODO: GPS esta activo
+
+    await Future.delayed(Duration(milliseconds: 1000));
+
+    Navigator.pushReplacement(context, navegarMapaFadeIn(context, MapaPage()));
+
+  }
+}
