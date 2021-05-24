b23ce782c810598bf65603fe683aabfd3de9e8cf
PerairaCC <carloscoto0103@gmail.com>
2021-05-23 18:58:57 -0600

    Clase 1 Seccion 18

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mapas_app/pages/acceso_gps_page.dart';
 import 'package:mapas_app/pages/loading_page.dart';
 import 'package:mapas_app/pages/mapa_page.dart';
+
+import 'bloc/mi_ubicacion/mi_ubicacion_bloc.dart';
  
 void main() => runApp(MyApp());
  
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    var materialApp = MaterialApp(
-      title: 'Material App',
-      debugShowCheckedModeBanner: false,
-      home: LoadingPage(),
-      routes: {
-        'mapa'       : ( _ ) => MapaPage(),
-        'loading'    : ( _ ) => LoadingPage(),
-        'acceso_gps' : ( _ ) => AccesoGpsPage(),
-      },
+    return MultiBlocProvider(
+      providers: [
+
+        BlocProvider(create: ( _ ) => MiUbicacionBloc()),
+
+      ],
+      child: MaterialApp(
+        title: 'Material App',
+        debugShowCheckedModeBanner: false,
+        home: LoadingPage(),
+        routes: {
+          'mapa'       : ( _ ) => MapaPage(),
+          'loading'    : ( _ ) => LoadingPage(),
+          'acceso_gps' : ( _ ) => AccesoGpsPage(),
+        },
+      ),
     );
-    return materialApp;
   }
 }
