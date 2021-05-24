2d2bbb0562ee25f5d342436ada9a7c7f747a2a1d
PerairaCC <carloscoto0103@gmail.com>
2021-05-23 19:17:07 -0600

    Clase 2 Seccion 18

--- lib/bloc/mi_ubicacion/mi_ubicacion_bloc.dart
+++ lib/bloc/mi_ubicacion/mi_ubicacion_bloc.dart
@@ -1,8 +1,9 @@
 import 'dart:async';
 
 import 'package:bloc/bloc.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:meta/meta.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart' show LatLng;
 
 part 'mi_ubicacion_event.dart';
 part 'mi_ubicacion_state.dart';
@@ -10,6 +11,23 @@ part 'mi_ubicacion_state.dart';
 class MiUbicacionBloc extends Bloc<MiUbicacionEvent, MiUbicacionState> {
 
   MiUbicacionBloc() : super(MiUbicacionState());
+
+  StreamSubscription<Position> _positionSubscription;
+
+  void iniciarSeguimiento() {
+
+    _positionSubscription = Geolocator.getPositionStream(
+      desiredAccuracy: LocationAccuracy.high,
+      distanceFilter: 10
+    ).listen((Position position) {
+      print(position);
+    });
+
+  }
+
+  void cancelarSeguimiento() {
+    this._positionSubscription?.cancel();
+  }
 
   @override
   Stream<MiUbicacionState> mapEventToState(MiUbicacionEvent event) async* {
