3cee2960c32389d80806ab2c6bd14672e167155b
PerairaCC <carloscoto0103@gmail.com>
2021-06-06 10:12:57 -0600

    Clase 5 Seccion 20

---
+++ lib/bloc/busqueda/busqueda_bloc.dart
@@ -0,0 +1,16 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+
+part 'busqueda_event.dart';
+part 'busqueda_state.dart';
+
+class BusquedaBloc extends Bloc<BusquedaEvent, BusquedaState> {
+  BusquedaBloc() : super(BusquedaState());
+
+  @override
+  Stream<BusquedaState> mapEventToState(BusquedaEvent event) async* {
+    
+  }
+}
